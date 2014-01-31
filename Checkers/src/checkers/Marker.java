d629f89c44a19d25bf9ac123b5856fd3e4d92844
Angela <Angela@Angela-Laptop>
2014-01-31 14:35:19 -0700

    Added function to Marker class to calculate number of pieces.

--- Checkers/src/checkers/Marker.java
+++ Checkers/src/checkers/Marker.java
@@ -12,16 +12,26 @@ package checkers;
  */
 public class Marker {    
     
-    char markerA = 'o';  
-    char markerB = 'x';
+    String markerA = "red";  
+    String markerB = "black";
+    double pieces = 0;
+    int rows = 8;
+    int columns = 12;
+
     
     public Marker(){
         
     }
     public void displayMarker(){
-        
+        //future insert - get column and row count inputs       
+        if (rows>6){
+        pieces = (int) ((columns/2)*3);
+        }
+        else pieces = (int) ((columns/2)*2);
         System.out.println("\n\tHere are the default pieces in the game: " + this.markerA + ", "
-                           + this.markerB);
+                           + this.markerB
+                           + "\n\n\tEach player will have " + pieces + " pieces for this board."
+                           + "\n");
     }
     
 }
