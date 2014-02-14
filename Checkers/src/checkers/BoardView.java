71a3e892b0a78c6e9e162fc090d832a1706bb344
BDawg <BDawg@192.168.0.10>
2014-02-14 11:51:25 -0700

    Individual Program for Brendon Young - BoardView Class

---
+++ Checkers/src/checkers/BoardView.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package checkers;
+
+/**
+ *
+ * @author BDawg
+ */
+public class BoardView {    
+   
+    int row = 8;
+    int column = 8;
+    
+    public BoardView() {           
+    
+    }
+    
+    void displayBoard(){
+        
+        this.printHeadRow();
+        this.printDividerRow();
+        for (int i = 0; i < row; i++) {           
+            this.printRow();             
+            this.printDividerRow();
+            System.out.print(i + 1);
+        }
+          
+        System.out.println();
+    }
+
+    private void printHeadRow() {       
+
+        System.out.print("\n\t      1   ");
+        int columnsInRow = column;
+        
+        for (int i = 1; i < columnsInRow - 1; i++) {
+            int col = i + 1;
+            System.out.print("  " + col + "   ");
+        }
+        System.out.print(" " + columnsInRow + "   ");
+    }
+
+    private void printDividerRow() {       
+
+        System.out.print("\n\t  |------");
+        int columnsInRow = column;
+        
+        for (int i = 1; i < columnsInRow - 1; i++) {
+            System.out.print("------");
+        }
+        System.out.print("-----|");
+    }
+
+    private void printRow() {
+        
+        // print first cell
+        String letter = " ";
+        
+        System.out.print("\n\t       " + " |  " + letter + "  |");
+        // print remaining cells in row
+        for (int i = 1; i < column-2; i++) {
+            
+            System.out.print("  " + letter + "  |");
+        }
+    }
+
+}
+
