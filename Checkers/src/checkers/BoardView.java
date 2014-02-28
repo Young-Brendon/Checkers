5dda71b19fa855009a1f287d60d471bade682fcc
Angela <Angela@Angela-Laptop>
2014-02-28 11:43:52 -0700

    Fixed row labels

--- Checkers/src/checkers/BoardView.java
+++ Checkers/src/checkers/BoardView.java
@@ -9,28 +9,28 @@ import java.util.Scanner;
 public class BoardView {    
  /*this initialize's the default layout of the board*/ 
     String[][] board = {
-        {"-","R","-","R","-","R","-","R"},
-        {"R","-","R","-","R","-","R","-"},
-        {"-","R","-","R","-","R","-","R"},
-        {" ","-"," ","-"," ","-"," ","-"},
-        {"-"," ","-"," ","-"," ","-"," "},
         {"B","-","B","-","B","-","B","-"},
         {"-","B","-","B","-","B","-","B"},
         {"B","-","B","-","B","-","B","-"},
+        {"-"," ","-"," ","-"," ","-"," "},
+        {" ","-"," ","-"," ","-"," ","-"},
+        {"-","R","-","R","-","R","-","R"},
+        {"R","-","R","-","R","-","R","-"},
+        {"-","R","-","R","-","R","-","R"},
     };
     
     void displayBoard() {
          /*adds the other characters to display the board.*/
           System.out.println("|--------------Checkers-------------|");
           System.out.println("|-----------------------------------|");
-          System.out.println("| 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 |"); 
+          System.out.println("| - | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 |"); 
           System.out.println("|-----------------------------------|");
-        for (int row=0;row<this.board.length;row++) {
-         
-       System.out.print("| " +  1 + " ");   
+        
+       for (int row=0;row<this.board.length;row++) {
+              System.out.print("| " +  (row+1) + " ");   
+          
           for(int col=0; col<this.board.length;col++){
-            
-        System.out.print("| " +this.board[row][col]+" ");
+              System.out.print("| " +this.board[row][col]+" ");
         
         }
           System.out.println("|");
@@ -39,62 +39,82 @@ public class BoardView {
         }
         this.selectPiece();
     }
-void selectPiece(){
-    /*this selects a piece according to the Coodranates given still needs alot of work*/
+    
+    
+public void selectPiece(){
+    /*this selects a piece according to the Coordinates given still needs alot of work*/
     Scanner input = new Scanner(System.in);
   
-    System.out.println("Please Select a piece According to your color you are");
-    System.out.print("Type in the Column for the piece: ");
- 
-    String x = input.next();
+    //System.out.println("Please Select a piece According to your color you are");
     System.out.print("Type in the Row for the piece: ");
-    input = new Scanner(System.in);
-    String y = input.next();
-    int x1 = Integer.parseInt(x);
+        input = new Scanner(System.in);
+            String y = input.next();
+    System.out.print("Type in the Column for the piece: ");
+        input = new Scanner(System.in);
+            String x = input.next();
     int y1 = Integer.parseInt(y);
-        switch (board[x1][y1]) {
-            case "R":
-                board[x1][y1] = " ";
-               this.movePiece();
-                break;
-            case "B":
-                 board[x1][y1] = " ";
-                 this.movePiece();
-                break;
-            default:
-                System.out.print("There is no Piece there"); 
-                        
-                break;
+    int x1 = Integer.parseInt(x);
+    
+        if((board[(x1-1)][(y1-1)]=="R") || (board[(x1-1)][(y1-1)]=="B")) {
+            board[(x1-1)][(y1-1)]=" ";
+            this.movePiece();
         }
-
+            else System.out.println("Invalid selection. Please try again.");
 }
-void movePiece(){
+
+        //switch (board[x1][y1]) {
+          //  case "R":
+            //    board[x1][y1] = " ";
+              // this.movePiece();
+                //break;
+            //case "B":
+              //   board[x1][y1] = " ";
+                // this.movePiece();
+               // break;
+           // default:
+             //   System.out.print("There is no Piece there"); 
+                        
+               // break;
+        //}
+
+
+public void movePiece(){
     /* this doesnt work but it is a start I think its supposed to draw the piece in its new location.*/
  Scanner input = new Scanner(System.in);
- System.out.print("Please indicate your color R or B");
- String color =input.next();
+// System.out.print("Please indicate your color R or B");
+// String color =input.next();
  System.out.print("Please input Where you would like to move this piece ");
  
- System.out.print("Type in the Column for the move: ");
- String x = input.next();
-    System.out.print("Type in the Row for the move: ");
+ System.out.print("Type in the Row for the piece: ");
     input = new Scanner(System.in);
-    String y = input.next();
-    int x1 = Integer.parseInt(x);
+       String y = input.next();
+ System.out.print("Type in the Column for the piece: ");
+    input = new Scanner(System.in);
+        String x = input.next();
     int y1 = Integer.parseInt(y);
-        switch (board[x1][y1]) {
-            case "-":
-                System.out.println("You cannot move here!");
-               this.movePiece();
-                break;
-            case " ":
-                 board[x1][y1] = color;              
-                break;
-            default:
-                System.out.print("there is already a Piece Here"); 
-                        
-                break;
+    int x1 = Integer.parseInt(x);
+    
+       if(board[(x1-1)][(y1-1)]==" ") {
+            //if(player=playerA){ ---- code to check for correct player
+                board[(x1-1)][(y1-1)]="M";
+                displayBoard();
+            //}
         }
+            else System.out.println("Invalid selection. Please try again.");
+        
+//        switch (board[x1][y1]) {
+//            case "-":
+//                System.out.println("You cannot move here!");
+//               this.movePiece();
+//                break;
+//            case " ":
+//                 board[x1][y1] = color;              
+//                break;
+//            default:
+//                System.out.print("there is already a Piece Here"); 
+//                        
+//                break;
+//        }
 
 }
 }
