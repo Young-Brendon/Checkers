00a9242eddd7ee213a6d165a840b71c41bdd8db6
Angela <Angela@Angela-Laptop>
2014-02-28 12:01:17 -0700

    Moves kind of working...

--- Checkers/src/checkers/BoardView.java
+++ Checkers/src/checkers/BoardView.java
@@ -52,71 +52,46 @@ public void selectPiece(){
     System.out.print("Type in the Column for the piece: ");
         input = new Scanner(System.in);
             String x = input.next();
-    int y1 = Integer.parseInt(y);
-    int x1 = Integer.parseInt(x);
+    int y2 = Integer.parseInt(y);
+    int x2 = Integer.parseInt(x);
+    int y1 = y2-1;
+    int x1 = x2-1;
     
-        if((board[(x1-1)][(y1-1)]=="R") || (board[(x1-1)][(y1-1)]=="B")) {
-            board[(x1-1)][(y1-1)]=" ";
+        if((board[(x1)][(y1)]=="R") || (board[(x1)][(y1)]=="B")) {
+            board[(x1)][(y1)]=" ";
             this.movePiece();
         }
             else System.out.println("Invalid selection. Please try again.");
 }
 
-        //switch (board[x1][y1]) {
-          //  case "R":
-            //    board[x1][y1] = " ";
-              // this.movePiece();
-                //break;
-            //case "B":
-              //   board[x1][y1] = " ";
-                // this.movePiece();
-               // break;
-           // default:
-             //   System.out.print("There is no Piece there"); 
-                        
-               // break;
-        //}
-
 
 public void movePiece(){
     /* this doesnt work but it is a start I think its supposed to draw the piece in its new location.*/
  Scanner input = new Scanner(System.in);
-// System.out.print("Please indicate your color R or B");
-// String color =input.next();
- System.out.print("Please input Where you would like to move this piece ");
+
+ System.out.println("Please input Where you would like to move this piece ");
  
- System.out.print("Type in the Row for the piece: ");
+ System.out.println("Type in the Row for the piece: ");
     input = new Scanner(System.in);
        String y = input.next();
  System.out.print("Type in the Column for the piece: ");
     input = new Scanner(System.in);
         String x = input.next();
-    int y1 = Integer.parseInt(y);
-    int x1 = Integer.parseInt(x);
+    int y2 = Integer.parseInt(y);
+    int x2 = Integer.parseInt(x);
+    int y1 = y2-1;
+    int x1 = x2-1;
     
-       if(board[(x1-1)][(y1-1)]==" ") {
+       if(((board[(x1)][(y1)]) != "R") || ((board[(x1)][(y1)]) != "B") || ((board[(x1)][(y1)]) != "-")) {
             //if(player=playerA){ ---- code to check for correct player
-                board[(x1-1)][(y1-1)]="M";
+                board[(x1)][(y1)]="M";
                 displayBoard();
             //}
         }
             else System.out.println("Invalid selection. Please try again.");
         
-//        switch (board[x1][y1]) {
-//            case "-":
-//                System.out.println("You cannot move here!");
-//               this.movePiece();
-//                break;
-//            case " ":
-//                 board[x1][y1] = color;              
-//                break;
-//            default:
-//                System.out.print("there is already a Piece Here"); 
-//                        
-//                break;
-//        }
 
-}
+    }
 }
 
 
