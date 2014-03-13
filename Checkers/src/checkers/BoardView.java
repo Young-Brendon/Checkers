4a43da7bcd9d5caf553dfaa0ea150b17f20c2887
Angela <Angela@Angela-Laptop>
2014-03-12 19:33:01 -0600

    Board now displaying correctly

--- Checkers/src/checkers/BoardView.java
+++ Checkers/src/checkers/BoardView.java
@@ -1,43 +1,25 @@
 
 package checkers;
 
-import java.util.Scanner;
 /**
   * @author Cody
  */
 public class BoardView { 
  
-   // Board board;
-    static final int Empty=0, Red=1, Black=2;
-    int[][] board;
+    Board board;
     
     public Object display(Object object) {
-         //adds the other characters to display the board.
-         // this.board = (Board) object;
-          board = new int[8][8];
-          this.printHeader();
-          setUpGame();
-          this.printDividerRow();
-          return board;
-    }
-    
-   void setUpGame(){
-      for (int rowCount = 0; rowCount < 8; rowCount++) {
-            for (int colCount = 0; colCount < 8; colCount++) {
-               if ( rowCount % 2 == colCount % 2 ) {
-                  if (rowCount < 3)
-                     board[rowCount][colCount] = Black;
-                  else if (rowCount > 4)
-                     board[rowCount][colCount] = Red;
-                  else
-                     board[rowCount][colCount] = Empty;
-               }
-               else {
-                  board[rowCount][colCount] = Empty;
-               }
-            }
-         }
-    }    
+        this.board = (Board) object;
+        this.printHeadRow();
+        for (int i = 0; i < this.board.getRowCount(); i++) {
+            Player[] rowOfLocations = this.board.getBoardLocations()[i];
+            this.printRow(i+1, rowOfLocations);
+            this.printDividerRow();
+        }
+        System.out.println();
+         
+        return null;
+    } 
           
           
  /*    String[][] board = {
@@ -53,17 +35,46 @@ public class BoardView {
      
     
     
-private void printHeader() {
-        System.out.println("|--------------Checkers-------------|");
+private void printHeadRow() {
+        System.out.println("|--------------Checkers--------------|");
           this.printDividerRow();
           //this.printHeadRow();
-          System.out.println("\n|   | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 |"); 
-          this.printDividerRow();     
+          System.out.println("\n| | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 |"); 
+          this.printDividerRow();
         } 
     
 private void printDividerRow() {
-        System.out.print("|-----------------------------------|");     
+
+        // print first cell
+
+        System.out.print("\n|");
+        int columnsInRow = this.board.getColumnCount();
+        // print remaining cells in row
+        for (int i = 1; i < columnsInRow - 1; i++) {
+            System.out.print("------");
         }
+        System.out.print("|");
+    }
+    
+    private void printRow(int rowNumber, Player[] rowLocations) {
+        
+        // print first cell
+        String letter = " ";
+        if (rowLocations[0] != null) {
+            letter = rowLocations[0].getMarker();
+        }
+        System.out.print("\n" + rowNumber + " | " + letter + " |");
+
+        // print remaining cells in row
+        for (int i = 1; i < rowLocations.length; i++) {
+            if (rowLocations[i] == null) {
+                letter = " ";
+            } else {
+                letter = rowLocations[i].getMarker();
+            }
+            System.out.print(" " + letter + " |");
+        }
+    }
 
 }
  /*      for (int i=0; i < this.board.getRowCount(); i++) {
